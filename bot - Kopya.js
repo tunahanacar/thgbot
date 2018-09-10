4307efeb8d6cf43de8f74802ac27835191219825
tunahanacar <43129312+tunahanacar@users.noreply.github.com>
2018-09-10 11:50:13 +0300

    Update bot - Kopya.js

--- bot - Kopya.js
+++ bot - Kopya.js
@@ -24,16 +24,6 @@ client.on('message', msg => {
     msg.channel.bulkDelete(100);
     msg.channel.sendMessage("100 adet mesaj silindi!");
   }
-  if (msg.content.toLowerCase() === prefix + 'reboot') {
-    if (msg.author.id !== ayarlar.sahip) {
-      msg.reply('Benim yapımcım değilsin!');
-    } else {
-      msg.channel.sendMessage(`Bot yeniden başlatılıyor...`).then(msg => {
-      console.log(`BOT: Bot yeniden başlatılıyor...`);
-      process.exit(0);
-    })
-   }
-  }
 });
 
 client.login(ayarlar.token);
