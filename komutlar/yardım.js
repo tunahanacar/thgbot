ebdbe625a5720c84f1a29ccba538047d16a1f691
tunahanacar <43129312+tunahanacar@users.noreply.github.com>
2018-09-12 17:42:05 +0300

    Update yardım.js

--- komutlar/yardım.js
+++ komutlar/yardım.js
@@ -8,7 +8,7 @@ exports.run = (client, message, params) => {
   .setTitle("THG [BOT] KOMUTLAR")
   .setDescription('')
   .setColor(0xff0000)
-  .addField("Yeni Komutlar:", 't!günaydın = Bahsedilen kişiye günaydın der. \n t!uyu = Uyursunuz. \n t!uçur = Bahsedilen kişiyi uçurur.')
+  .addField("Yeni Komutlar:", 't!çekirdek \n t!günaydın = Bahsedilen kişiye günaydın der. \n t!uyu = Uyursunuz. \n t!uçur = Bahsedilen kişiyi uçurur.')
   .addField("Eğlence Komutları:", `t!banned = Banlama şakası yapar. \n t!stresçarkı = Stres çarkı çevirir. \n t!öp = Belirtilen kişiyi öper. \n t!sarıl = Belirtilen kişiye sarılır. \n t!tokalaş = Belirtilen kişiyle tokalaşır \n t!yumruh-at = Yumruk Atarsınız. \n t!yaz = Bota İstediğiniz Şeyi Yazdırırsınız. \n t!emojiyazı = Emoji şeklinde yazı yazarsınız. \n t!sorusor = Sorduğunuz soruya cevap verir. \n t!türk = Türk olduğunu kanıtlar. \n t!hapis = Birisini hapse atarsın.`)
   .addField("Kullanıcı Komutları:" , 't!avatarım = Avatarınınızı Gösterir. \n t!döviz = Döviz durmunu gösterir. \n t!sunucubilgi = BOT Sunucu Hakkında Bilgi Verir. \n t!kullanıcıbilgim = Sizin Hakkınızda Bilgi Verir. \n t!sex = NSFW Komutudur. \n t!ass = NSFW Komutudur.')
   .addField("Sunucu Yetkilisi Komutları", `t!ban = İstediğiniz Kişiyi Sunucudan Banlar. \n t!kick  = İstediğiniz Kişiyi Sunucudan Atar. \n t!unban = İstediğiniz Kişinin Yasağını Açar. \n t!sustur = İstediğiniz Kişiyi Susturur. -MUTED- \n t!oylama = Oylama Açar. \n t!duyuru = Güzel Bir Duyuru Görünümü Sağlar.`)
