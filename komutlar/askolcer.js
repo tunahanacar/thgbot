6b29cfdaa1e6a112877456cf8798c3ee85b5de32
tunahanacar <43129312+tunahanacar@users.noreply.github.com>
2019-01-25 12:36:32 +0300

    Add files via upload

---
+++ komutlar/askolcer.js
@@ -0,0 +1,32 @@
+const Discord = require('discord.js');
+
+const cevaplar = [
+    ":heart: :heart: :heart: :heart: :heart: %100 | Gerçekten seviyorsun.",
+    ":heart: :heart: :heart: :heart: %75 | Hoşlanıyorsun :slime:",
+    ":heart: :heart: :heart: %50 | Yarı yarıya değilmi ? :angry:",
+    ":heart: :heart: %20 | Gerçekten ciddi sıkıntılarınız var. :rage:",
+    "%0 | Agam gösterge bozuldu heralde :stuck_out_tongue_closed_eyes: ",
+];
+
+exports.run = function(client, message, args) {
+    var soru = args.join(' ');
+
+    var cevap = cevaplar[Math.floor(Math.random() * cevaplar.length)];
+
+    if(!ask) return message.reply('Birisini belirt. **Doğru Kullanım**: t!askolcer <kişi>')
+    else message.channel.send(cevap)
+
+};  
+
+exports.conf = {
+  enabled: true, 
+  guildOnly: true, 
+  aliases: [],
+  permLevel: 0 
+};
+
+exports.help = {
+  name: 'askolcer', 
+  description: 'THR BOT askolcer',
+  usage: 'askolcer <kişi>'
+};
