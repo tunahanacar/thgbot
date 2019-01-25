c79e7a5f3b365e04eb6391c33fc9a8ab87e81975
tunahanacar <43129312+tunahanacar@users.noreply.github.com>
2019-01-25 12:40:03 +0300

    Update askolcer.js

--- komutlar/askolcer.js
+++ komutlar/askolcer.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 
 const cevaplar = [
     ":heart: :heart: :heart: :heart: :heart: %100 | Gerçekten seviyorsun.",
-    ":heart: :heart: :heart: :heart: %75 | Hoşlanıyorsun :slime:",
+    ":heart: :heart: :heart: :heart: %75 | Hoşlanıyorsun :smile:",
     ":heart: :heart: :heart: %50 | Yarı yarıya değilmi ? :angry:",
     ":heart: :heart: %20 | Gerçekten ciddi sıkıntılarınız var. :rage:",
     "%0 | Agam gösterge bozuldu heralde :stuck_out_tongue_closed_eyes: ",
