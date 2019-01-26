cb444d2fec7cdab0b10f121627619c9f09cf0e1c
tunahanacar <43129312+tunahanacar@users.noreply.github.com>
2019-01-26 11:32:50 +0300

    Update askolcer.js

--- komutlar/askolcer.js
+++ komutlar/askolcer.js
@@ -3,10 +3,13 @@ const Discord = require('discord.js');
 const cevaplar = [
     ":heart: :heart: :heart: :heart: :heart: **%100** | OMG bu sonuç ney lan :smile:",
     ":heart: :heart: :heart: :heart: :heart: **%98** | Yaa sen ciddimisin :heart:",
+    ":heart: :heart: :heart: :heart: **%83** | Güzel işte böyle ! :heart:",
     ":heart: :heart: :heart: :heart: **%75** | Hoşlanıyorsun :smile:",
+    ":heart: :heart: :heart: :heart: **%70** | Hoşlanıyorsun :smile:",
     ":heart: :heart: :heart: :heart: **%88** | Sen ciddisin böyle devam et!! :smile:",
     ":heart: :heart: :heart: **%50** | Yarı yarıya değilmi ? :angry:",
-    ":heart: :heart: :heart: **%67** | İyi gidiyorsun ? :slight_smile:",
+    ":heart: :heart: :heart: **%52** | Morelini kaybetme , olacak. :neutral_face:",
+    ":heart: :heart: :heart: **%67** | İyi gidiyorsun. :slight_smile:",
     ":heart: :heart: :heart: **%57** | Hoşuna gidiyorsun , yolunu kaybetme ? :slight_smile:",
 ];
 
