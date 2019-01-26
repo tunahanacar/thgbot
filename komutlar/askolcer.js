9c0e6e184a272f10f15633eaae0ec9f387a4d3e1
tunahanacar <43129312+tunahanacar@users.noreply.github.com>
2019-01-26 11:25:22 +0300

    Update askolcer.js

--- komutlar/askolcer.js
+++ komutlar/askolcer.js
@@ -1,13 +1,13 @@
 const Discord = require('discord.js');
 
 const cevaplar = [
-    ":heart: :heart: :heart: :heart: :heart: %100 | OMG bu sonuç ney lan :smile:",
-    ":heart: :heart: :heart: :heart: :heart: %98 | Yaa sen ciddimisin :heart:",
-    ":heart: :heart: :heart: :heart: %75 | Hoşlanıyorsun :smile:",
-    ":heart: :heart: :heart: :heart: %88 | Sen ciddisin böyle devam et!! :smile:",
-    ":heart: :heart: :heart: %50 | Yarı yarıya değilmi ? :angry:",
-    ":heart: :heart: :heart: %67 | İyi gidiyorsun ? :slight_smile:",
-    ":heart: :heart: :heart: %57 | Hoşuna gidiyorsun , yolunu kaybetme ? :slight_smile:",
+    ":heart: :heart: :heart: :heart: :heart: **%100** | OMG bu sonuç ney lan :smile:",
+    ":heart: :heart: :heart: :heart: :heart: **%98** | Yaa sen ciddimisin :heart:",
+    ":heart: :heart: :heart: :heart: **%75** | Hoşlanıyorsun :smile:",
+    ":heart: :heart: :heart: :heart: **%88** | Sen ciddisin böyle devam et!! :smile:",
+    ":heart: :heart: :heart: **%50** | Yarı yarıya değilmi ? :angry:",
+    ":heart: :heart: :heart: **%67** | İyi gidiyorsun ? :slight_smile:",
+    ":heart: :heart: :heart: **%57** | Hoşuna gidiyorsun , yolunu kaybetme ? :slight_smile:",
 ];
 
 exports.run = function(client, message, args) {
